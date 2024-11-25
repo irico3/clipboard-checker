7004c4667992aecddfcbf4ec12ffd24fc5dd83f8
kana-takano@cybozu.co.jp <iricocco3@gmail.com>
2024-11-25 16:48:19 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by k002265 on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
