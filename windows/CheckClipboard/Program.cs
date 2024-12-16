4f77dd57ce016c5e8a74dd132b056cb2b69d8a2e
irico3 <iricocco3@gmail.com>
2024-12-16 21:18:46 +0900

    windowsを追加

---
+++ windows/CheckClipboard/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Windows.Forms;
+
+
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        try
+        {
+            IDataObject data = Clipboard.GetDataObject();
+
+            if (data != null)
+            {
+                foreach (var format in data.GetFormats())
+                {
+
+                    Console.WriteLine(format);
+                }
+            }
+            else
+            {
+                Console.WriteLine("クリップボードにデータがありません");
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        Console.ReadKey();
+
+
+    }
+
+}
